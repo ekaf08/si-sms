48ace505f924bbbe500d3cd4b0e62de776436b23
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2023-08-09 13:21:37 +0700

    menambahkan template admin LTE V-3

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,7 @@
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        @yield('content')
+    </div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('layouts.app');
 });
