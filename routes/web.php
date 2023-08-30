c9d2f95de8a689f6519152fa0b88db1e118353c1
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2023-08-30 11:05:57 +0700

    menambahkan middleware untuk login

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,34 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'authLogin'])->name('authLogin');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-});
+// Route::get('/admin/dashboard', function () {
+//     return view('admin.dashboard');
+// });
 
 Route::get('/admin/list', function () {
     return view('admin.admin.list');
+});
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'student'], function () {
+    Route::get('/student/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('/teacher/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('/parent/dashboard', function () {
+        return view('admin.dashboard');
+    });
 });
